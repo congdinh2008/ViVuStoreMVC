78654bf578b95f81e2a5496a47bb1111d88a8ec1
Cong Dinh <congdinh2012@hotmail.com>
2018-06-23 08:55:36 +0700

    Create project and add bootstrap, jquery, popper.js by libman

---
+++ ViVuStoreMVC/ViVuStoreMVC/Data/ViVuStoreDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ViVuStoreMVC.Data
+{
+    public class ViVuStoreDbContext : DbContext
+    {
+        public ViVuStoreDbContext(DbContextOptions<ViVuStoreDbContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
